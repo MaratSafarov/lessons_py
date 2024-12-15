0087888bc26a95ea041ef793b04faad432101e7f
MaratSafarov <marat1620@gmail.com>
2024-12-15 18:59:08 +0300

    1

---
+++ m5-classes-and-objects/module_5_1.py
@@ -0,0 +1,21 @@
+class House:
+
+    def __init__(self, name, qty):
+        self.name = name
+        self.number_of_floors = qty
+
+        def go_to(new_floor):
+            self.new_floor = int(qty)
+
+            print(go_to(len[1:new_floor]))
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+#h1.go_to(5)
+#h2.go_to(10)
+
+print(h1)
+print(h2)
+
+print(House)
