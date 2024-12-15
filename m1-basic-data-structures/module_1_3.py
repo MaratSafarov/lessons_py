6bd61fc035ba513c8ab4ac5b2dc61a461b54f80c
MaratSafarov <marat1620@gmail.com>
2024-12-15 12:35:02 +0300

    Add files via upload

---
+++ m1-basic-data-structures/module_1_3.py
@@ -0,0 +1,15 @@
+name = 'Marat'
+print (name)
+age = 38
+print (age)
+new_age = 1
+print (age + new_age)
+is_student = age > new_age
+print (is_student)
+
+userName = 'Marat'
+userAge = 38
+userNew_age = age + 1
+userIs_student = age > new_age
+user = (f'\nName: {userName} \nAge: {userAge} \nNew_age: {userNew_age} \nIs_student: {userIs_student}')
+print (user)
