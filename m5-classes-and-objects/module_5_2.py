1c5a483e9e69246193288b54da34cdb6b9e11db2
MaratSafarov <marat1620@gmail.com>
2024-12-16 18:54:52 +0300

    Специальные методы классов

--- m5-classes-and-objects/module_5_2.py
+++ m5-classes-and-objects/module_5_2.py
@@ -14,7 +14,7 @@ class House:
         return self.number_of_floors
 
     def __str__(self):
-        return str(f"Название: {self.name}, кол-во этажей: {self.number_of_floors}")
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
 
 
 h1 = House('ЖК Эльбрус', 10)
