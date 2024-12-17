4f1f7a52c97b9ae3b6a58724a4d8ee8aaf2953dd
MaratSafarov <marat1620@gmail.com>
2024-12-17 23:43:24 +0300

    перегрузка операторов

--- m5-classes-and-objects/module_5_3.py
+++ m5-classes-and-objects/module_5_3.py
@@ -12,6 +12,7 @@ class House:
 
     def __len__(self):
         return self.number_of_floors
+
     def __str__(self):
         return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
 
@@ -24,25 +25,31 @@ class House:
 
     def __eq__(self, other):
         return self.comparison_(other, lambda x, y: x == y)
+
     def __lt__(self, other):
         return self.comparison_(other, lambda x, y: x < y)
+
     def __le__(self, other):
         return self.comparison_(other, lambda x, y: x <= y)
+
     def __gt__(self, other):
         return self.comparison_(other, lambda x, y: x > y)
+
     def __ge__(self, other):
         return self.comparison_(other, lambda x, y: x >= y)
+
     def __ne__(self, other):
         return self.comparison_(other, lambda x, y: x != y)
-
 
     def __add__(self, value):
         if isinstance(value, int) and value > 0:
             return House(self.name, self.number_of_floors + value)
         return self
+
     def __radd__(self, value):
         self.number_of_floors += value
         return self
+
     def __iadd__(self, value):
         self.number_of_floors += value
         return self
@@ -54,18 +61,18 @@ h2 = House('ЖК Акация', 20)
 print(h1)
 print(h2)
 
-print(h1 == h2) # __eq__
-h1 = h1 + 10 # __add__
+print(h1 == h2)  # __eq__
+h1 = h1 + 10  # __add__
 print(h1)
 print(h1 == h2)
 
-h1 += 10 # __iadd__
+h1 += 10  # __iadd__
 print(h1)
-h2 = 10 + h2 # __radd__
+h2 = 10 + h2  # __radd__
 print(h2)
 
-print(h1 > h2) # __gt__
-print(h1 >= h2) # __ge__
-print(h1 < h2) # __lt__
-print(h1 <= h2) # __le__
-print(h1 != h2) # __ne__
+print(h1 > h2)  # __gt__
+print(h1 >= h2)  # __ge__
+print(h1 < h2)  # __lt__
+print(h1 <= h2)  # __le__
+print(h1 != h2)  # __ne__
