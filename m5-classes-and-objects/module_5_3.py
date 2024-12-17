b2645c10d1107e9e397fbc016658ce8c3e6aa275
MaratSafarov <marat1620@gmail.com>
2024-12-17 20:01:01 +0300

    перегрузка операторов

--- m5-classes-and-objects/module_5_3.py
+++ m5-classes-and-objects/module_5_3.py
@@ -15,22 +15,30 @@ class House:
     def __str__(self):
         return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
 
+    def comparison_(self, other, operator):
+        if isinstance(other, House):
+            return operator(self.number_of_floors, other.number_of_floors)
+        if isinstance(other, int):
+            return operator(self.number_of_floors, other)
+        return NotImplemented
+
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
+        return self.comparison_(other, lambda x, y: x == y)
     def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
+        return self.comparison_(other, lambda x, y: x < y)
     def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
+        return self.comparison_(other, lambda x, y: x <= y)
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
+        return self.comparison_(other, lambda x, y: x > y)
     def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
+        return self.comparison_(other, lambda x, y: x >= y)
     def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
+        return self.comparison_(other, lambda x, y: x != y)
+
 
     def __add__(self, value):
-        if value > 0:
-            self.number_of_floors += value
+        if isinstance(value, int) and value > 0:
+            return House(self.name, self.number_of_floors + value)
         return self
     def __radd__(self, value):
         self.number_of_floors += value
@@ -47,14 +55,12 @@ print(h1)
 print(h2)
 
 print(h1 == h2) # __eq__
-
 h1 = h1 + 10 # __add__
 print(h1)
 print(h1 == h2)
 
 h1 += 10 # __iadd__
 print(h1)
-
 h2 = 10 + h2 # __radd__
 print(h2)
 
